1d2572a3833d1d5ee4c0b36b52a439326220e270
Abhinab <abhinabacharya303@gmail.com>
2022-10-02 22:31:13 -0400

    epoch and batch size

--- model/multi_head.py
+++ model/multi_head.py
@@ -90,8 +90,9 @@ class MutliHead(nn.Module):
 
     def forward(self, x):
         # print("WEIGHT: ", self.weight.shape)
-        self.weight = torch.nn.Parameter(self.p_norm(self.weight, 0.5))
-        normed_w = self.multi_head_call(self.causal_norm, self.weight, weight=self.norm_scale)
+        # self.weight = torch.nn.Parameter(self.p_norm(self.weight, 0.5))
+        normed_w = torch.nn.Parameter(self.p_norm(self.weight, 1))
+        # normed_w = self.multi_head_call(self.causal_norm, self.weight, weight=self.norm_scale)
         normed_x = self.multi_head_call(self.l2_norm, x)
         y = torch.mm(normed_x * self.scale, normed_w.t())
 
